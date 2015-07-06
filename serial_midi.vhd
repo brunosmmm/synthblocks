dabc8e53357a6473a56a708a6e77c3ac0d272d6b
brunosmmm <brunosmmm@gmail.com>
2015-07-06 08:55:20 -0400

    Communications unit skeleton added

---
+++ serial_midi.vhd
@@ -0,0 +1,27 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity serial_midi is
+  port(ck : in std_logic;
+       rst : in std_logic;
+       tx : out std_logic;
+       rx: in std_logic;
+
+       --control bus
+       ctl_addr : out std_logic_vector(15 downto 0);
+       ctl_data_out : out std_logic_vector(15 downto 0);
+       ctl_data_in : in std_logic_vector(15 downto 0);
+       ctl_rd : out std_logic;
+       ctl_wr : out std_logic
+       
+       );
+
+end entity;
+
+architecture comm of serial_midi is
+begin
+
+
+
+end architecture;
