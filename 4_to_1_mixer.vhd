31752607aedb11d993e41224620bd6e45a1c62a1
brunosmmm <brunosmmm@gmail.com>
2015-07-13 22:35:31 -0400

    Adjustable gain resolution & proper fixed point

--- 4_to_1_mixer.vhd
+++ 4_to_1_mixer.vhd
@@ -3,7 +3,8 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity mix_4_to_1 is
-  generic(data_depth : integer := 24);
+  generic(data_depth : integer := 24;
+          gain_res : integer := 16);
   port(in_1 : in std_logic_vector(data_depth-1 downto 0);
        in_2 : in std_logic_vector(data_depth-1 downto 0);
        in_3 : in std_logic_vector(data_depth-1 downto 0);
@@ -11,10 +12,10 @@ entity mix_4_to_1 is
 
        mix_out : out std_logic_vector(data_depth-1 downto 0);
 
-       gain_1 : in std_logic_vector(15 downto 0);
-       gain_2 : in std_logic_vector(15 downto 0);
-       gain_3 : in std_logic_vector(15 downto 0);
-       gain_4 : in std_logic_vector(15 downto 0)
+       gain_1 : in std_logic_vector(gain_res-1 downto 0);
+       gain_2 : in std_logic_vector(gain_res-1 downto 0);
+       gain_3 : in std_logic_vector(gain_res-1 downto 0);
+       gain_4 : in std_logic_vector(gain_res-1 downto 0)
        );
 end entity;
 
@@ -32,22 +33,26 @@ begin
   --gain blocks
 
   g1: entity work.gain(modify)
-    generic map(data_depth=>data_depth)
+    generic map(data_depth=>data_depth,
+                gain_res=>gain_res)
     port map(data_in=>in_1,
              gain_in=>gain_1,
              data_out=>in_1_g);
   g2: entity work.gain(modify)
-    generic map(data_depth=>data_depth)
+    generic map(data_depth=>data_depth,
+                gain_res=>gain_res)
     port map(data_in=>in_2,
              gain_in=>gain_2,
              data_out=>in_2_g);
   g3: entity work.gain(modify)
-    generic map(data_depth=>data_depth)
+    generic map(data_depth=>data_depth,
+                gain_res=>gain_res)
     port map(data_in=>in_3,
              gain_in=>gain_3,
              data_out=>in_3_g);
   g4: entity work.gain(modify)
-    generic map(data_depth=>data_depth)
+    generic map(data_depth=>data_depth,
+                gain_res=>gain_res)
     port map(data_in=>in_4,
              gain_in=>gain_4,
              data_out=>in_4_g);
--- gain.vhd
+++ gain.vhd
@@ -25,22 +25,27 @@ architecture modify of gain is
   --velocity mode
   --multiplier table
   constant vm_tab : vel_mult_table := generate_vel_mult;
+  constant env_tab : env_mult_table := generate_env_mult;
+  
   signal velocity_multiplier : ufixed(2 downto -5);
+  signal envelope_multiplier : ufixed(2 downto -5);
   signal data_times_vel : signed(data_depth-1 downto 0);
+  signal data_times_env : signed(data_depth-1 downto 0);
   
   signal data_in_f : sfixed(data_depth-1 downto -5);
   --interpret as fixed point directly
-  signal gain_in_f : sfixed(gain_res/2-1 downto -gain_res);
+  signal gain_in_f : sfixed(gain_res-6 downto -5);
   
 begin
 
 
   data_out <= std_logic_vector(data_times_vel) when gain_mode = gain_mode_velocity else
+              std_logic_vector(data_times_env) when gain_mode = gain_mode_envelope else
               std_logic_vector(calculate_normal);
 
   --data_in to fixed
   data_in_f <= to_sfixed(signed(data_in), data_in_f);
-  gain_in_f <= to_sfixed(signed(gain_in), gain_in_f);
+  gain_in_f <= to_sfixed(gain_in, gain_in_f);
   
   --calculate directly
   calculate_normal <= to_signed(data_in_f * gain_in_f, calculate_normal);
@@ -48,9 +53,15 @@ begin
   --scale with velocity
 
   --velocity multiplier
-  velocity_multiplier <= vm_tab(to_integer(unsigned(gain_in)));
+  velocity_multiplier <= vm_tab(to_integer(unsigned(gain_in))) when gain_mode = gain_mode_velocity else
+                         (others=>'0');
   data_times_vel <= to_signed(data_in_f * to_sfixed(velocity_multiplier),
                               data_times_vel);
+
+  envelope_multiplier <= env_tab(to_integer(unsigned(gain_in))) when gain_mode = gain_mode_envelope else
+                         (others=>'0');
+  data_times_env <= to_signed(data_in_f * to_sfixed(envelope_multiplier),
+                              data_times_env);
 
 
 end architecture;
