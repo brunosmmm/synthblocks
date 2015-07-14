211b56cf500d02a16fcb12e0381f2f0374546c16
brunosmmm <brunosmmm@gmail.com>
2015-07-13 22:38:42 -0400

    Voice state machine for note handling

--- voice.vhd
+++ voice.vhd
@@ -2,6 +2,9 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+library ieee_proposed;
+use ieee_proposed.fixed_pkg.all;
+
 use work.sb_common.all;
 
 entity voice is
@@ -75,10 +78,10 @@ architecture sound of voice is
   signal opmat_outd : std_logic_vector(data_depth-1 downto 0);
 
   --mixer signals
-  signal gain_a : std_logic_vector(15 downto 0);
-  signal gain_b : std_logic_vector(15 downto 0);
-  signal gain_c : std_logic_vector(15 downto 0);
-  signal gain_d : std_logic_vector(15 downto 0);
+  signal gain_a : std_logic_vector(7 downto 0);
+  signal gain_b : std_logic_vector(7 downto 0);
+  signal gain_c : std_logic_vector(7 downto 0);
+  signal gain_d : std_logic_vector(7 downto 0);
   signal mixer_out : std_logic_vector(data_depth-1 downto 0);
 
   --control unit signals
@@ -100,21 +103,66 @@ architecture sound of voice is
   
     --ADSR envelope generator
   signal adsr_out : std_logic_vector(data_depth-1 downto 0);
-  alias attack : std_logic_vector(6 downto 0) is attack_in(6 downto 0);
-  alias decay : std_logic_vector(6 downto 0) is decay_in(6 downto 0);
-  alias release : std_logic_vector(6 downto 0) is release_in(6 downto 0);
-  alias sustain : std_logic_vector(6 downto 0) is sustain_in(6 downto 0);
+  alias attack : std_logic_vector(7 downto 0) is attack_in(7 downto 0);
+  alias decay : std_logic_vector(7 downto 0) is decay_in(7 downto 0);
+  alias release : std_logic_vector(7 downto 0) is release_in(7 downto 0);
+  alias sustain : std_logic_vector(7 downto 0) is sustain_in(7 downto 0);
+  signal noteoff : std_logic;
+  signal releasing : std_logic;
+
+  --save pitch and velocity
+  signal v_pitch : std_logic_vector(6 downto 0);
+  signal v_vel : std_logic_vector(6 downto 0);
+
+  type vp_save is (vp_idle, vp_active, vp_releasing);
   
 begin
 
+  --register pitch & velocity
+  save: process(clk_100, rst, active)
+    variable save_state : vp_save;
+  begin
+
+    if rst = '1' then
+      save_state := vp_idle;
+      v_pitch <= (others=>'0');
+      v_vel <= (others=>'0');
+    elsif rising_edge(clk_100) then
+      case save_state is
+        when vp_idle =>
+
+          if active = '1' then
+            v_vel <= velocity;
+            v_pitch <= pitch;
+            save_state := vp_active;
+          end if;
+
+        when vp_active =>
+
+          --wait for release
+          if releasing = '1' then
+            save_state := vp_releasing;
+          end if;
+
+        when vp_releasing =>
+          if releasing = '0' then
+            --finished
+            save_state := vp_idle;
+          end if;
+      end case;
+      
+    end if;
+
+  end process;
+  
   --velocity mode
-  osc1_gain <= velocity when param_velocity_gain = '1' else
+  osc1_gain <= v_vel when param_velocity_gain = '1' else
                (others=>'1'); --maximum
-  osc2_gain <= velocity when param_velocity_gain = '1' else
+  osc2_gain <= v_vel when param_velocity_gain = '1' else
                (others=>'1');
-  osc3_gain <= velocity when param_velocity_gain = '1' else
+  osc3_gain <= v_vel when param_velocity_gain = '1' else
                (others=>'1');
-  osc4_gain <= velocity when param_velocity_gain = '1' else
+  osc4_gain <= v_vel when param_velocity_gain = '1' else
                (others=>'1');
 
   --control unit
@@ -145,25 +193,25 @@ begin
   osc1_shamt <= osc_1_2_shamt(7 downto 0);
   osc1_shift: entity work.pitch_shifter(shift)
     port map(shamt=>osc1_shamt,
-             pitch_in=>pitch,
+             pitch_in=>v_pitch,
              pitch_out=>osc1_spitch);
 
   osc2_shamt <= osc_1_2_shamt(15 downto 8);
   osc2_shift: entity work.pitch_shifter(shift)
     port map(shamt=>osc2_shamt,
-             pitch_in=>pitch,
+             pitch_in=>v_pitch,
              pitch_out=>osc2_spitch);
 
   osc3_shamt <= osc_3_4_shamt(7 downto 0);
   osc3_shift: entity work.pitch_shifter(shift)
     port map(shamt=>osc3_shamt,
-             pitch_in=>pitch,
+             pitch_in=>v_pitch,
              pitch_out=>osc3_spitch);
 
   osc4_shamt <= osc_3_4_shamt(15 downto 8);
   osc4_shift: entity work.pitch_shifter(shift)
     port map(shamt=>osc4_shamt,
-             pitch_in=>pitch,
+             pitch_in=>v_pitch,
              pitch_out=>osc4_spitch);
   
   osc1_sel <= osc_sel(2 downto 0);
@@ -250,9 +298,16 @@ begin
              out_c=>opmat_outc,
              out_d=>opmat_outd);
 
+
+  gain_a <= to_slv(to_sfixed(0.5, 2, -5));
+  gain_b <= to_slv(to_sfixed(0.5, 2, -5));
+  gain_c <= (others=>'0');
+  gain_d <= (others=>'0');
+  
   --mix oscillators
   mix0: entity work.mix_4_to_1(mix)
-    generic map(data_depth=>data_depth)
+    generic map(data_depth=>data_depth,
+                gain_res=>8)
     port map(in_1=>opmat_outa,
              in_2=>opmat_outb,
              in_3=>opmat_outc,
@@ -266,7 +321,9 @@ begin
 				 
 				 
 	--missing filter!
-	
+
+  noteoff <= not active;
+  
 	--envelope generator
   adsr0: entity work.adsr_gen(envelope)
     generic map(data_depth=>data_depth)
@@ -275,14 +332,15 @@ begin
              data_in=>mixer_out,
              data_out=>adsr_out,
              note_on=>active,
-             note_off=>not active,
+             note_off=>noteoff,
              attack_i=>attack,
              decay_d=>decay,
              release_d=>release,
-             sustain_l=>sustain);
+             sustain_l=>sustain,
+             releasing=>releasing);
   
 
-  data_out <= adsr_out when active = '1' else
+  data_out <= adsr_out when active = '1' or releasing = '1' else
               (others=>'0');
   
 end architecture;
